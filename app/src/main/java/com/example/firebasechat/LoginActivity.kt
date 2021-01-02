6a0600086cb695d402eb69dcddb1c75040c4e4db
Dmitry Shkadarevich <41323462+KastusKalinovski@users.noreply.github.com>
2021-01-02 15:55:45 +0300

    Add SignUp and Login

---
+++ app/src/main/java/com/example/firebasechat/LoginActivity.kt
@@ -0,0 +1,67 @@
+package com.example.firebasechat
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth? = null
+    private var firebaseUser: FirebaseUser? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                Toast.makeText(
+                    applicationContext,
+                    "email and password are required",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                auth!!.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) {
+                        if (it.isSuccessful) {
+                            etEmail.setText("")
+                            etPassword.setText("")
+                            val intent = Intent(
+                                this@LoginActivity,
+                                HomeActivity::class.java
+                            )
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                "email or password invalid",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+            }
+        }
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(
+                this@LoginActivity,
+                SignUpActivity::class.java
+            )
+            startActivity(intent)
+            finish()
+        }
+    }
+}
